a5c6ece72415763436e0d14dea78dda8a04569bd
Denys07748 <denis07748@gmail.com>
2023-03-10 21:49:22 +0200

    Додає компонент Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,13 @@
 import { Profile } from "./Profile";
+import { Statistics } from "./Statistics";
+import { FriendList } from "./FriendList";
+import {TransactionHistory} from "./TransactionHistory";
 import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
+
 
 export const App = () => {
   return (
@@ -21,6 +29,19 @@ export const App = () => {
       followers={user.stats.followers}
       views={user.stats.views}
       likes={user.stats.likes}
+      />
+
+      <Statistics
+      title="Upload stats"
+      stats={data}
+      />
+
+      <FriendList
+      friends={friends}
+      />
+
+      <TransactionHistory
+      items={transactions}
       />
     </div>
   );
