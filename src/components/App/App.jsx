0e69853de14f2bbc02523dffef0c06bb73d4aa12
Denys07748 <denis07748@gmail.com>
2023-03-14 22:09:49 +0200

    додає бібліотеку Emotion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,18 @@
-import { Profile } from "./profile/Profile";
-import { Statistics } from "./statistics/Statistics";
-import { FriendList } from "./friends/FriendList";
-import { TransactionHistory } from "./transactions/TransactionHistory";
+import { Profile } from "../profile/Profile";
+import { Statistics } from "../statistics/Statistics";
+import { FriendList } from "../friends/FriendList";
+import { TransactionHistory } from "../transactions/TransactionHistory";
+import { Container } from "./App.styled";
+
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
 
 
-
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        fontSize: 20,
-        color: '#010101'
-      }}
-    >
+    <Container>
       <Profile 
       username={user.username}
       tag={user.tag}
@@ -40,6 +35,6 @@ export const App = () => {
       <TransactionHistory
       items={transactions}
       />
-    </div>
+    </Container>
   );
 };
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    height: '100vh',
+    fontSize: 20,
+    color: '#010101'
+`
